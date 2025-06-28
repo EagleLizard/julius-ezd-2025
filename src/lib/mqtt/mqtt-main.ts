fe6a77c091dd37abc1185df5b54b53d9bba8622f
eaglelizard <whitedtd@gmail.com>
2025-06-28 15:26:04 -0600

    add rudimentary arg parsing

---
+++ src/lib/mqtt/mqtt-main.ts
@@ -0,0 +1,4 @@
+
+export async function mqttMain() {
+  console.log('mqttMain');
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,19 @@
+/* this should remain the first import */
+import 'source-map-support/register';
+
+import assert from 'node:assert';
+
+const cmd_map = {
+  mqtt: 'mqtt',
+  etc: 'etc',
+} as const;
+assert(Object.entries(cmd_map).every(([ key, val ]) => {
+  return key === val;
+}));
+type JuliusCommand = keyof typeof cmd_map;
+type JuliusArgs = {
+  cmd: JuliusCommand;
+} & {};
 
 (async () => {
   try {
@@ -10,10 +26,49 @@
 
 async function main() {
   setProcName();
+  let jArgs = parseArgs();
+  switch(jArgs?.cmd) {
+    case cmd_map.mqtt:
+      await (await import('./lib/mqtt/mqtt-main')).mqttMain();
+      break;
+    case cmd_map.etc:
+      console.log('etc');
+      break;
+    case undefined:
+      printCmds();
+  }
 }
 
-function parseArgs() {
+function printCmds() {
+  let outLines: string[] = [];
+  let cmdStrs: string[] = [ ...Object.keys(cmd_map) ];
+  outLines.push('commands:');
+  outLines.push(`  ${cmdStrs.join(', ')}`);
+  process.stdout.write(`${outLines.join('\n')}\n`);
+}
+
+/*
+  keep it simple for now, parse the command str
+_*/
+function parseArgs(): JuliusArgs | undefined {
   let args: string[];
+  let firstArg: string;
+  let jArgs: JuliusArgs;
+  args = process.argv.slice(2);
+  if(args.length < 1) {
+    return;
+  }
+  firstArg = args[0];
+  if(!checkJuliusCommand(firstArg)) {
+    return;
+  }
+  jArgs = {
+    cmd: firstArg,
+  };
+  return jArgs;
+}
+function checkJuliusCommand(cmdStr: string): cmdStr is JuliusCommand {
+  return ([ ...Object.values(cmd_map) ] as readonly string[]).includes(cmdStr);
 }
 
 function setProcName() {
